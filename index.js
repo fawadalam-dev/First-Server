3ce38d9cf9cbafd7aafb260b2ae64220e30b9f20
Fawad Alam <fawadalam5813@gmail.com>
2026-03-05 01:57:43 +0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import express from 'express';
+import cors from 'cors';
+
 
 const app = express();
-app.get('/home', (req, res) => {
-    res.send('Hello World');
+
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST'],
+}));
+let products = [
+    {
+        id: 1,
+        name: 'Dell Laptop',
+        imageURL: "",
+        price: 10.99,
+        description: 'This is Dell Laptop',
+    },
+
+    {
+        id: 2,
+        name: 'HP Laptop',
+        imageURL: "",
+        price: 12.99,
+        description: 'This is HP Laptop',
+    },
+];
+app.get('/products', (req, res) => {
+    res.json(products);
 });
-app.get('/about', (req, res) => {
-    res.send('This is about page');
-});
-app.get('/weather', (req, res) => {
-    res.send('Yes today is 30 degree');
+app.post('/products', (req, res) => {
+    const newProduct = req.body;
+    products.push(newProduct);
+    res.status(201).json(newProduct);
 });
 app.listen(8000, () => {
-   console.log('Server is Running');
+    console.log('Server is Running on port 8000');
 });
